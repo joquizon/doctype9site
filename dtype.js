87c15ec0b9f014fbadcc3462244d7115084be138
Jose Quizon <josequizon@hotmail.com>
2017-04-18 16:29:45 -0400

    update

--- dtype.js
+++ dtype.js
@@ -6,5 +6,42 @@ $(document).ready(function()
 			$(this).toggleClass("solobuttA");
 		});
 
+	// code for laocoon
+			setInterval(function clocker()
+		{
+			var month = [
+			  "January",
+			"February",
+			"March",
+			"April",
+			 "May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December"];
+
+			var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat",]
+			var currentTime = new Date ( );
+			var td = currentTime.getDate();
+			var tm = currentTime.getMonth();
+			var ty = currentTime.getFullYear();
+			var tdy = currentTime.getDay();
+			var th = currentTime.getHours();
+			var tmt = currentTime.getMinutes();
+			var ts = currentTime.getSeconds();
+			var n = currentTime.getMilliseconds();
+			var tmm = currentTime.getTime();
+			var monthL = month[tm]
+			var day = day[tdy]
+			var browser = "BROWSER// "+ navigator.appName + " "+navigator.appCodeName;
+			var timelist = td +" "+" "+ty+" "+" "+ th +" ' "+ tmt +" '' "+ ts+ " ''' "+n+"*";
+			$(".dater").html(timelist);
+
+
+		},1);
+
 
 });
